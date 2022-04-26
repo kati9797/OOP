86210e7a2f588af25f60bea370001d162c2ffb68
Ekaterina <102674934+kati9797@users.noreply.github.com>
2022-04-27 00:56:08 +0300

    Add files via upload

---
+++ Tickets/Hall.cpp
@@ -0,0 +1,99 @@
+#include "Hall.h"
+
+// Конструктор по подразбиране 
+
+Hall::Hall()
+{
+	number = 0;
+	rows = 0;
+	seats = 0;
+
+	allSeats = 0;
+	freeSeats = 0;
+}
+
+// Конструктор с параметри
+
+Hall::Hall(int num, int rows, int seats)
+{
+	setNumber(num);
+	setRows(rows);
+	setSeats(seats);
+
+	allSeats = rows * seats;
+	freeSeats = allSeats;
+}
+
+// Мутатори
+
+void Hall::setNumber(int num)
+{
+	if (num < 0)
+	{
+		number = 0;
+	}
+	else
+	{
+		number = num;
+	}
+}
+
+void Hall::setRows(int rows)
+{
+	if (rows < 0)
+	{
+		this->rows = 0;
+	}
+	else 
+	{
+		this->rows = rows;
+	}
+}
+
+void Hall::setSeats(int seats)
+{
+	if (seats < 0)
+	{
+		this->seats = 0;
+	}
+	else
+	{
+		this->seats = seats;
+	}
+}
+
+// Селектори
+
+int Hall::getNumber() const
+{
+	return number;
+}
+
+int Hall::getRows() const
+{
+	return rows;
+}
+
+int Hall::getSeats() const
+{
+	return seats;
+}
+
+int Hall::getAllSeats() const
+{
+	return allSeats;
+}
+
+int Hall::getFreeSeats() const
+{
+	return freeSeats;
+}
+
+// Функция за извеждане
+
+void Hall::printHall() const
+{
+	std::cout << "Number: " << number << ", Rows: " << rows << ", Seats: " << seats << std::endl;
+}
+
+
