8128035a0c0303d03e34a14829fa03d2d7552f68
kati9797 <102674934+kati9797@users.noreply.github.com>
2022-04-27 00:53:19 +0300

    test.hall

---
+++ Tickets/Hall.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#ifndef HALL_H
+#define HALL_H
+
+// Клас Зала
+class Hall
+{
+private:
+
+	// Номер на зала
+	int number;
+	//Брой редове в залата
+	int rows;
+	// Брой места на един ред
+	int seats;
+	
+	// Допълнителна член-променлива за общия брой места в залата
+	int allSeats;
+	// Допълнителна член-променлива за свобободните места в залата
+	int freeSeats;
+
+public:
+
+	// Конструктори
+	Hall();
+	Hall(int, int, int);
+
+	// Мутатори
+	void setNumber(int);
+	void setRows(int);
+	void setSeats(int);
+
+	// Селектори
+	int getNumber() const;
+	int getRows() const;
+	int getSeats() const;
+	int getAllSeats() const;
+	int getFreeSeats() const;
+
+	// Функция за извеждане
+	void printHall() const;
+};
+
+
+#endif // !HALL_H
+
+
