92e923c9fad5f46510a888ff69850821ad5332c9
Ekaterina <102674934+kati9797@users.noreply.github.com>
2022-04-27 00:44:15 +0300

    Update Date.h

--- Tickets/Date.h
+++ Tickets/Date.h
@@ -1,25 +1,40 @@
 #ifndef DATE_H
 #define DATE_H
 
+// Клас Дата
 class Date
 {
 private:
+
 	int day;
 	int month;
 	int year;
 
+	// Масив, пазещ максималния брой дни за всеки месец от годината
 	int maxDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 public:
+
+	// Конструктори
 	Date();
 	Date(int, int, int);
+
+	// Мутатори
 	void setDay(int);
 	void setMonth(int);
 	void setYear(int);
+
+	// Селектори
 	int getDay() const;
 	int getMonth() const;
 	int getYear() const;
+
+	// Функция за извеждане
 	void printDate() const;
 };
 
+// Оператор за сравнение на дати
 bool operator==(const Date&, const Date&);
-#endif
+{
+	return (date1.getDay() == date2.getDay() && date1.getMonth() == date2.getMonth() && date1.getYear() == date2.getYear());
+}
