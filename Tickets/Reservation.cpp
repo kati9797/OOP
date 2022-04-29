e37895abbe9c8754a0e69ad3ef6dbf3b1f926c88
Ekaterina <atanasova150@gmail.com>
2022-04-30 00:30:16 +0300

    Update Reservation.cpp

--- Tickets/Reservation.cpp
+++ Tickets/Reservation.cpp
@@ -1,25 +1,5 @@
 #include "Reservation.h"
 
-// Копираща функция
-
-void Reservation::copyFrom(const Reservation& other)
-{
-	hall = other.hall;
-	row = other.row;
-	seat = other.seat;
-	sizePass = other.sizePass;
-	password = new char[sizePass + 1];
-	strcpy(password, other.password);
-	strcpy(note, other.note);
-}
-
-// Освобождаване на динамично заделената памет
-
-void Reservation::free()
-{
-	delete[] password;
-}
-
 // Конструктор по подразбиране
 
 Reservation::Reservation()
@@ -27,14 +7,12 @@ Reservation::Reservation()
 	hall = Hall();
 	row = 0;
 	seat = 0;
-	password = nullptr;
-	sizePass = 0;
-	strcpy(note, "\0");
+	password = MyString();
 }
 
 // Конструктор с параметри
 
-Reservation::Reservation(int newRow, int newSeat, const char* pass)
+Reservation::Reservation(int newRow, int newSeat, const MyString pass)
 {
 	hall = Hall();
 	row = newRow;
@@ -42,43 +20,11 @@ Reservation::Reservation(int newRow, int newSeat, const char* pass)
 	setPass(pass);
 }
 
-//Копиращ конструктор
-
-Reservation::Reservation(const Reservation& other)
-{
-	copyFrom(other);
-}
-
-//Оператор "="
-
-Reservation& Reservation::operator=(const Reservation& other)
-{
-	if (this != &other)
-	{
-		free();
-		copyFrom(other);
-	}
-	return *this;
-}
-
-// Деструктор
-
-Reservation::~Reservation()
-{
-	free();
-}
-
 // Мутатори
 
-void Reservation::setPass(const char* pass)
+void Reservation::setPass(const MyString& pass)
 {
-	if (password != nullptr)
-	{
-		free();
-	}
-	sizePass = strlen(pass);
-	password = new char[sizePass + 1];
-	strcpy(password, pass);
+	password = pass;
 }
 
 void Reservation::setNote(char* newNote)
@@ -93,7 +39,7 @@ void Reservation::setHall(const Hall& newHall)
 
 // Селектори
 
-char* Reservation::getPass() const
+MyString Reservation::getPass() const
 {
 	return password;
 }
@@ -117,5 +63,5 @@ Hall Reservation::getHall() const
 
 void Reservation::printReservation() const
 {
-	std::cout << row << " " << seat << " " << password << " " << note;
+	std::cout << row << " " << seat << " " << password.getString() << std::endl;
 }
