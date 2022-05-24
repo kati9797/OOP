9e0026078f65aaeb721a2db7aff30204b168c158
Ekaterina <atanasova150@gmail.com>
2022-05-24 15:20:27 +0300

    Create Minivan.h

---
+++ Garage/Minivan.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Car.h"
+const int maxSpeedVan = 160;
+const int minSpeedVan = 120;
+
+class Minivan : public Vehicle
+{
+private:
+
+	int speed;
+
+public:
+	Minivan() = default;
+	Minivan(const char*, int);
+
+	void setSpeed(int);
+	int getSpeed() const;
+	void print() const;
+};
