0c419021c9e949bc9807a6fc714480eb176a8e25
Ekaterina <atanasova150@gmail.com>
2022-05-24 22:22:50 +0300

    Update Minivan.cpp

--- Garage/Minivan.cpp
+++ Garage/Minivan.cpp
@@ -1,8 +1,19 @@
 #include "Minivan.h"
 
+Minivan::Minivan()
+{
+	speed = minSpeedVan;
+}
+
 Minivan ::Minivan(const char* brand, int speed) : Vehicle(brand)
 {
 	setSpeed(speed);
+}
+
+Vehicle* Minivan::clone() const
+{
+	Vehicle* newObj = new Minivan(*this);
+	return newObj;
 }
 
 void Minivan::setSpeed(int speed)
@@ -20,6 +31,11 @@ void Minivan::setSpeed(int speed)
 int Minivan::getSpeed() const
 {
 	return speed;
+}
+
+int Minivan::getMaxSpeed() const
+{
+	return maxSpeedVan;
 }
 
 void Minivan::print() const
