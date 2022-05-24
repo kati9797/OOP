382d9d5d4fc00a16df346615999eb355666853ff
Ekaterina <atanasova150@gmail.com>
2022-05-24 15:21:24 +0300

    Create Truck.h

---
+++ Garage/Truck.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Minivan.h"
+const int maxSpeedTruck = 140;
+const int minSpeedTruck = 80;
+
+class Truck : public Vehicle
+{
+private:
+
+	int speed;
+
+public:
+	Truck() = default;
+	Truck(const char*, int);
+
+	void setSpeed(int);
+	int getSpeed() const;
+	void print() const;
+};
